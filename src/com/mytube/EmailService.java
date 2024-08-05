84e5d0021c925b1dfb17b1a94200d922e3f8d0cd
briann-git <brian.mburu.ndungu@gmail.com>
2024-08-05 22:28:04 +0100

    Added @Override to interface method implementations

--- src/com/mytube/EmailService.java
+++ src/com/mytube/EmailService.java
@@ -1,6 +1,7 @@
 package com.mytube;
 
 public class EmailService implements CanSendMessage {
+    @Override
     public void sendMessageTo(User user) {
         System.out.println("Notifying " + user.getEmail() + "...");
         System.out.println("Done!\n");
--- src/com/mytube/VideoEncoder.java
+++ src/com/mytube/VideoEncoder.java
@@ -1,6 +1,7 @@
 package com.mytube;
 
 public class VideoEncoder implements CanEncodeVideo {
+    @Override
     public void encode(Video video) {
         System.out.println("Encoding video...");
         System.out.println("Done!\n");
